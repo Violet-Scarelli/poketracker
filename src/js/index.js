41f77b6b0d2706afec98418c4eedfc9ef5d513eb
Violet-Scarelli <alphaone150@gmail.com>
2023-05-18 12:46:56 -0700

    updated, first framework

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+class Poketracker{
+    constructor(){
+        this.pokeApi = "https://pokeapi.co/api/v2/pokemon/";
+        this.statApi = "https://pokeapi.co/api/v2/stat/";
+    }
+    getPokemon(){
+
+    }
+    savePokemon(){
+
+    }
+    
+}
