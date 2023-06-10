6f6d0df20ef964f6b0479e869471963a5bf10e1e
Violet-Scarelli <alphaone150@gmail.com>
2023-06-09 17:19:32 -0700

    lots and lots of changes, app is in a semi-usable state now

--- src/js/pokemon.js
+++ src/js/pokemon.js
@@ -1,5 +1,26 @@
-class Pokemon{
-    constructor(){
-        
+import Pokedex from 'pokedex-promise-v2';
+const options = {
+  protocol: 'https',
+  hostName: 'pokeapi.co',
+  versionPath: '/api/v2/',
+  cacheLimit: 100 * 1000, // 100s
+  timeout: 5 * 1000 // 5s
+}
+const P = new Pokedex(options);
+export default class Pokemon {
+    constructor() {
+        this.pokemon;
+    }
+    async getPokemon(pokeId) {
+        try{
+        const pokemon = await P.getPokemonByName(pokeId)
+        .then(data => {
+            console.log(data);
+            return data;
+        })
+        }
+        catch (error) {
+            throw error;
+        }
     }
 }
